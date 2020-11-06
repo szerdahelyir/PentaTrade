7f22fde04e2591d49a4e7a59dc8c476f48b52e27
szerdahelyir <47789479+szerdahelyir@users.noreply.github.com>
2020-11-06 11:13:54 +0100

    Delete search-result.ts

--- src/app/search-result.ts
+++
@@ -1,9 +0,0 @@
-export class SearchResult {
-    constructor(
-        public track: string,
-        public artist: string,
-        public link: string,
-        public thumbnail: string,
-        public artistId: string
-      ) {}
-}
